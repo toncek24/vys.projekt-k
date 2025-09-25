dffd1aa3ee62e94cbd11d89af122d7c20ea53108
Antonín Vídeňský <videnskya24@spstrutnov.cz>
2025-09-25 08:17:15 +0200

    Kámen nůžky papír
    začátek

---
+++ kámen.cs
@@ -0,0 +1,18 @@
+﻿namespace vys.projekt_
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            string[] moznosti = { "kámen", "nůžky", "papír" };
+            Random rnd = new Random();
+
+            Console.Write("Vyber si Kámen, nůžky, nebo papír!");
+            string VolbaHrace = Console.Readline().To.Lower();
+
+            if (!Array.Exists(moznosti, prvek => prvek == VolbaHrace))
+
+
+        }
+    }
+}
