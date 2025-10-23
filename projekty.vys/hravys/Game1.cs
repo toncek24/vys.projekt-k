d84c58ec20a0cba253ecde5c99462926e694b04f
Antonín Vídeňský <videnskya24@spstrutnov.cz>
2025-10-23 08:31:14 +0200

    wall

--- projekty.vys/hravys/Game1.cs
+++ projekty.vys/hravys/Game1.cs
@@ -14,6 +14,8 @@ namespace hravys
         private Vector2 _clankerPosition;
         private int _clankerSize = 50;
         private float _speed = 5f;
+        private Texture2D _wallTexture;
+        private Rectangle _wallRect;
 
         public Game1()
         {
@@ -35,10 +37,22 @@ namespace hravys
         protected override void LoadContent()
         {
             _spriteBatch = new SpriteBatch(GraphicsDevice);
-
+            _wallTexture = new Texture2D(GraphicsDevice, 1, 1);
+            _wallTexture.SetData(new[] { Color.Black });
             _clankerTexture = new Texture2D(GraphicsDevice, 1, 1);
             _clankerTexture.SetData(new[] { Color.CornflowerBlue });
 
+            int wallWidth = 200;
+            int wallHeight = 40;
+            int screenWidth = _graphics.PreferredBackBufferWidth;
+            int screenHeight = _graphics.PreferredBackBufferHeight;
+
+            _wallRect = new Rectangle(
+                (screenWidth / 2) + 150,  // move 150px right from center
+                (screenHeight / 2) - (wallHeight / 2),
+                wallWidth,
+                wallHeight
+            );
         }
 
         protected override void Update(GameTime gameTime)
@@ -59,6 +73,23 @@ namespace hravys
             _clankerPosition.Y = MathHelper.Clamp(_clankerPosition.Y, 0, _graphics.PreferredBackBufferHeight - _clankerSize);
 
             base.Update(gameTime);
+
+            Rectangle clankerRect = new Rectangle(
+                (int)_clankerPosition.X,
+                (int)_clankerPosition.Y,
+                _clankerSize,
+                _clankerSize
+            ); ;
+
+                        // If colliding with wall, push clanker back
+                        if (clankerRect.Intersects(_wallRect))
+                        {
+                            // Simple collision resolution: move clanker back opposite its velocity
+                            if (k.IsKeyDown(Keys.W)) _clankerPosition.Y += _speed;
+                            if (k.IsKeyDown(Keys.S)) _clankerPosition.Y -= _speed;
+                            if (k.IsKeyDown(Keys.A)) _clankerPosition.X += _speed;
+                            if (k.IsKeyDown(Keys.D)) _clankerPosition.X -= _speed;
+                        }
         }
 
         protected override void Draw(GameTime gameTime)
@@ -66,6 +97,8 @@ namespace hravys
             GraphicsDevice.Clear(Color.White);
 
             _spriteBatch.Begin();
+
+            _spriteBatch.Draw(_wallTexture, _wallRect, Color.Gray);
 
             _spriteBatch.Draw(
                 _clankerTexture,
